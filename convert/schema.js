9f2ec491821e38195e956172118d0d5bad7d85fb
vancarney <carney.van@gmail.com>
2017-01-04 09:47:33 -0800

    cleaned up files

--- convert/schema.js
+++
@@ -1,8 +0,0 @@
-class Convert {
-    //### @set(key, value)
-    //> sets key/value to virtualized _object
-
-
-function __guard__(value, transform) {
-  return (typeof value !== 'undefined' && value !== null) ? transform(value) : undefined;
-}
