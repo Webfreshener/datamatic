da435de70f0fbba62adb747d35125163f1b61e80
vancarney <carney.van@gmail.com>
2018-01-31 11:50:52 -0600

    updated dev-deps

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,12 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
+    context: path.join(__dirname),
     entry: './src/index.js',
     output: {
-        // libraryTarget: 'var',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.join(__dirname, 'dist'),
         filename: 'jsd.js',
         library: 'JSD',
         libraryTarget: 'umd'
-    }
+    },
 };
