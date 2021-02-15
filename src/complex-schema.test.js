0f107785d9bb44717fd2af7271b08ca404b05183
Van Schroeder <vanschroeder@mesmer.local>
2021-02-14 19:46:28 -0600

    refactored exec error handler

--- src/complex-schema.test.js
+++ src/complex-schema.test.js
@@ -1,6 +1,7 @@
 import {default as Schema} from "../fixtures/basic-refs.schema";
 import {default as Data} from "../fixtures/basic-refs.data";
-import {default as OpenAPIv2} from "../fixtures/OpenAPIv2";
+import {default as Swagger} from "../fixtures/OpenAPIv2";
+import {default as OpenAPIv3} from "../fixtures/OpenAPIv3";
 import {default as JSONSchema4} from "../fixtures/json-schema-draft04.schema";
 import {default as basicAPI} from "../fixtures/petstore.v2"
 import {Model} from "./Model";
@@ -24,18 +25,39 @@ describe("Basic Refs", () => {
     });
 
     describe("OPenAPI Tests", () => {
-        it("should load and validate OpenAPIv2 Schema", () => {
+        it("should load and validate Swagger Schema", () => {
             const _owner = new Model({
                 meta: [JSONSchema4],
-                schemas: [OpenAPIv2],
-                use: "http://swagger.io/v2/schema.json#"
+                schemas: [Swagger],
+                use: "http://swagger.io/v2/schema.json#",
             });
-                _owner.model = basicAPI;
+            _owner.model = basicAPI;
             // test to ensure no errors were logged
             expect(_owner.errors).toBe(null);
             // test for completeness
             expect(_owner.toJSON()).toEqual(basicAPI);
-        })
-            ;
-        })
+        });
+
+        it("should load and validate OpenAPIv3 Schema", () => {
+            console.log("should load and validate OpenAPIv3 Schema");
+            const _d = {
+                openapi: "3.0.1",
+                info: {
+                    title: "testing",
+                    version: "1.0.0",
+                },
+                paths: {},
+            };
+            const _owner = new Model({
+                meta: [JSONSchema4],
+                schemas: [OpenAPIv3],
+                use: "http://openapis.org/v3/schema.json#",
+            });
+            _owner.model = _d;
+            // test to ensure no errors were logged
+            expect(_owner.errors).toBe(null);
+            // test for completeness
+            expect(_owner.toJSON()).toEqual(_d);
+        });
     });
+});
