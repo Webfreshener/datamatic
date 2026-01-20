983e9648d5f0c66ae701e0050a14ad1ba82ee911
Webfreshener LLC <van@webfreshener.com>
2026-01-20 02:03:38 -0700

    nice formatting of demos

--- integration/commonjs-node/index.js
+++ integration/commonjs-node/index.js
@@ -1,4 +1,26 @@
-const {Pipeline} = require("datamatic");
-const pipeline = new Pipeline({type: "array", items: {type: "string"}});
-pipeline.subscribe((d) => console.log(d), (e) => console.log(e));
-pipeline.write(["a", "b", "c"]);
+const { Pipeline } = require('datamatic');
+
+const accent = (text) => `\x1b[38;5;82m${text}\x1b[0m`;
+const dim = (text) => `\x1b[2m${text}\x1b[0m`;
+
+const pipeline = new Pipeline({
+  type: 'array',
+  items: {
+    type: 'string'
+  }
+});
+
+console.log('');
+console.log(accent('Datamatic CommonJS Integration'));
+console.log(dim('='.repeat(32)));
+
+pipeline.subscribe({
+  next: (data) => {
+    console.log(accent('Pipeline output:'), data);
+  },
+  error: (error) => {
+    console.error(accent('Pipeline error:'), error);
+  }
+});
+
+pipeline.write(['alpha', 'beta', 'gamma']);
